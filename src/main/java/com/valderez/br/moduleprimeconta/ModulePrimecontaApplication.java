1f37c0e3c6a4efc99018ba3d0545821175b4f853
valderez <valderezpe@gmail.com>
2024-01-12 12:01:55 -0300

    feat: config project

---
+++ src/main/java/com/valderez/br/moduleprimeconta/ModulePrimecontaApplication.java
@@ -0,0 +1,13 @@
+package com.valderez.br.moduleprimeconta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulePrimecontaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModulePrimecontaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valderez/br/moduleprimeconta/ModulePrimecontaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valderez.br.moduleprimeconta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulePrimecontaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
