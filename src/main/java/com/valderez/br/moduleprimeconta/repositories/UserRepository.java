097632f282f54835319a5e93325e36cceb98c526
valderez <valderezpe@gmail.com>
2024-01-12 12:04:25 -0300

    feat: create UserRepository

---
+++ src/main/java/com/valderez/br/moduleprimeconta/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.valderez.br.moduleprimeconta.repositories;
+
+import com.valderez.br.moduleprimeconta.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+     Optional<User> findUserByDocument(String document);
+    Optional<User> findUserById(Long id);
+}
